cc41fb60be1cbd1457ad12b70a65b0390ab1c507
zahirul <zahirulopel@gmail.com>
2025-04-13 12:45:56 +0600

    users and post schema created succesfully

--- config/database.js
+++ config/database.js
@@ -7,6 +7,34 @@ console.log(connectionString);
 
 const pool = new Pool({ connectionString });
 
+const createDbTable = async () => {
+  const createTableQuery = `CREATE TABLE IF NOT EXISTS users(
+  user_id SERIAL PRIMARY KEY,
+  first_name VARCHAR (50) NOT NULL,
+  last_name VARCHAR (50) NOT NULL,
+  username VARCHAR (50) UNIQUE NOT NULL,
+  password VARCHAR (50) NOT NULL,
+  is_admin BOOLEAN NOT NULL,
+  is_member BOOLEAN NOT NULL
+  );
+
+  CREATE TABLE IF NOT EXISTS posts(
+  post_id SERIAL PRIMARY KEY,
+  user_id INT,
+  title VARCHAR (255) NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+  post TEXT NOT NULL,
+  CONSTRAINT fk_posts FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE
+  );
+  `;
+  try {
+    await pool.query(createTableQuery);
+    console.log("Users and posts table created successfully.");
+  } catch (error) {
+    console.error("Error on creating users and posts table,", error);
+  }
+};
+
 const connectToDb = async () => {
   try {
     const { rows } = await pool.query("SELECT version();");
@@ -16,6 +44,7 @@ const connectToDb = async () => {
     console.error("Error on connecting to db:", error);
   }
 };
-
 connectToDb();
+createDbTable();
+
 module.exports = new Pool({ connectionString });
