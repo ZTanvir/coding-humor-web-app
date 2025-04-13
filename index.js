c4007ac8de2f22881013438c988f59f575a0e040
zahirul <zahirulopel@gmail.com>
2025-04-13 12:23:09 +0600

    add footer and navbar views

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
+const path = require("path");
 const db = require("./config/database");
 
 const app = express();
 app.use(morgan("dev"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", async (req, res) => {
+  res.render("home", { title: "Coding Humor" });
+});
 
 module.exports = app;
