441ccbac53cbbfe8fb3c36ca61f6e82831ee01de
zahirul <zahirulopel@gmail.com>
2025-04-21 12:09:36 +0600

    add error msg on sign up form

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,7 +1,35 @@
+const { body, validationResult } = require("express-validator");
+
+const signupValidator = [
+  body("username", "Username can't be empty.").not().isEmpty(),
+  body("password", "The minimum password length is 6 characters.").isLength({
+    min: 6,
+  }),
+  body("confirmPassword")
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage("Password don't match."),
+  body("fname", "First name can't be empty.").not().isEmpty(),
+  body("lname", "Last name can't be empty.").not().isEmpty(),
+];
+
 const getUserRegistrationForm = async (req, res) => {
-  res.render("../views/pages/registration-page", {});
+  res.render("../views/pages/registration-page", { errors: [] });
 };
 
-const postUserRegistrationForm = async (req, res) => {};
+const postUserRegistrationForm = async (req, res) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return res.status(200).json({ msg: "No errors" });
+  }
+  res.render("../views/pages/registration-page", {
+    errors: errors["errors"],
+  });
+};
 
-module.exports = { getUserRegistrationForm, postUserRegistrationForm };
+module.exports = {
+  getUserRegistrationForm,
+  postUserRegistrationForm,
+  signupValidator,
+};
--- routers/users.js
+++ routers/users.js
@@ -2,6 +2,10 @@ const userRoute = require("express").Router();
 const userControllers = require("../controllers/usersControllers");
 
 userRoute.get("/sign-up", userControllers.getUserRegistrationForm);
-userRoute.post("/sign-up", userControllers.postUserRegistrationForm);
+userRoute.post(
+  "/sign-up",
+  userControllers.signupValidator,
+  userControllers.postUserRegistrationForm
+);
 
 module.exports = userRoute;
