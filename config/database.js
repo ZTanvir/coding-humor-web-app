4e4fab689c0f7ad021a37cedebe7e340d7aebac2
zahirul <zahirulopel@gmail.com>
2025-04-21 12:43:18 +0600

    add new register user to the db with hash and salted password

--- config/database.js
+++ config/database.js
@@ -13,7 +13,7 @@ const createDbTable = async () => {
   first_name VARCHAR (50) NOT NULL,
   last_name VARCHAR (50) NOT NULL,
   username VARCHAR (50) UNIQUE NOT NULL,
-  password VARCHAR (50) NOT NULL,
+  password VARCHAR (255) NOT NULL,
   is_admin BOOLEAN NOT NULL,
   is_member BOOLEAN NOT NULL
   );
--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,4 +1,6 @@
 const { body, validationResult } = require("express-validator");
+const db = require("../config/database");
+const bcrypt = require("bcryptjs");
 
 const signupValidator = [
   body("username", "Username can't be empty.").not().isEmpty(),
@@ -23,6 +25,18 @@ const postUserRegistrationForm = async (req, res) => {
   console.log(errors);
 
   if (errors.isEmpty()) {
+    const { username, password, fname, lname } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+    try {
+      await db.query(
+        "INSERT INTO users(first_name,last_name,username,password,is_admin,is_member) VALUES($1,$2,$3,$4,$5,$6)",
+        [fname, lname, username, hashPassword, false, false]
+      );
+    } catch (error) {
+      console.log("Error while adding user to users table", error);
+    }
+
     return res.status(200).json({ form: req.body });
   }
   res.render("../views/pages/registration-page", {
