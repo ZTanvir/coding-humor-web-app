64dd106f278d5e5d9d8ad199bfe60f92e7934446
zahirul <zahirulopel@gmail.com>
2025-04-23 12:24:21 +0600

    add express-session

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
+const expressSession = require("express-session");
+const pgSession = require("connect-pg-simple")(expressSession);
 const db = require("./config/database");
 const usersRouter = require("./routers/users");
+const config = require("./utils/config");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -12,6 +15,20 @@ app.set("public", path.join(__dirname, "public"));
 app.use(morgan("dev"));
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  expressSession({
+    store: new pgSession({
+      pool: db, // Connection pool
+      tableName: "session", // Use another table-name than the default "session" one
+      // Insert connect-pg-simple options here
+    }),
+    secret: config.FOO_COOKIE_SECRET,
+    resave: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
+    // Insert express-session options here
+    saveUninitialized: false,
+  })
+);
 
 app.use("/auth", usersRouter);
 
--- utils/config.js
+++ utils/config.js
@@ -2,4 +2,6 @@ require("dotenv").config();
 const pgConnectionString = process.env.PG_CONNECTION_STRING;
 const port = process.env.PORT;
 
-module.exports = { pgConnectionString, port };
+const FOO_COOKIE_SECRET = process.env.FOO_COOKIE_SECRET;
+
+module.exports = { pgConnectionString, port, FOO_COOKIE_SECRET };
