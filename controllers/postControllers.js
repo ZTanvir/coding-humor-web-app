8a95d34195110111a1388b82d7f0a438016d6df2
zahirul <zahirulopel@gmail.com>
2025-05-10 12:27:30 +0600

    add error msg and populate input with previous input

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,4 +1,22 @@
 const db = require("../config/database");
+const { body, validationResult } = require("express-validator");
+
+const validatePost = [
+  body("post_title")
+    .not()
+    .isEmpty()
+    .withMessage("Please enter post title")
+    .trim()
+    .escape(),
+  body("post_descriptions")
+    .not()
+    .isEmpty()
+    .withMessage("Please add your post")
+    .isLength({ max: 10 })
+    .withMessage("Post must be within 1000 character.")
+    .trim()
+    .escape(),
+];
 
 const getPosts = async (req, res) => {
   const { rows } = await db.query(
@@ -6,7 +24,29 @@ const getPosts = async (req, res) => {
   );
   const posts = [...rows];
 
-  return res.render("pages/posts-page", { posts });
+  return res.render("pages/posts-page", { posts, errors: [], postData: {} });
 };
 
-module.exports = { getPosts };
+const addPost = async (req, res) => {
+  console.log("I am add post router", req.body);
+  const { post_title, post_descriptions } = req.body;
+  const { rows } = await db.query(
+    "SELECT username,post_id,title,created_at,post FROM users JOIN posts ON users.user_id=posts.user_id;"
+  );
+  const posts = [...rows];
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.render("pages/posts-page", {
+      posts,
+      errors: errors.array(),
+      postData: {
+        post_title,
+        post_descriptions,
+      },
+    });
+  }
+};
+
+module.exports = { getPosts, addPost, validatePost };
--- routers/posts.js
+++ routers/posts.js
@@ -4,4 +4,10 @@ const postControllers = require("../controllers/postControllers");
 
 postRoutes.get("/posts", postControllers.getPosts);
 
+postRoutes.post(
+  "/posts",
+  postControllers.validatePost,
+  postControllers.addPost
+);
+
 module.exports = postRoutes;
