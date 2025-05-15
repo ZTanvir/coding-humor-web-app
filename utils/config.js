882b1b7a85e7e6453c4046bb0d58650022537deb
zahirul <zahirulopel@gmail.com>
2025-05-15 15:44:31 +0600

    add process env production to config

--- utils/config.js
+++ utils/config.js
@@ -4,6 +4,8 @@ if (process.env.NODE_ENV === "developmentLocal") {
   pgConnectionString = process.env.PG_CONNECTION_STRING;
 } else if (process.env.NODE_ENV === "developmentVercel") {
   pgConnectionString = process.env.VERCEL_DATABASE_URL;
+} else if (process.env.NODE_ENV === "production") {
+  pgConnectionString = process.env.VERCEL_DATABASE_URL;
 }
 const port = process.env.PORT;
 
