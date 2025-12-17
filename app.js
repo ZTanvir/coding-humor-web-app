b57e5d3cb5b29ebb86a464621dee8014b5ecfae7
zahirul <zahirulopel@gmail.com>
2025-12-17 14:04:28 +0600

    add a pull request

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const config = require("./utils/config");
 
 const PORT = config.port || 3001;
 
+// a sample comment has been added to see pull request
+
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
 });
